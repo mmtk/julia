6875767c3dc6ff72d72a1fdf4e7988896e6211af
Yi Lin <qinsoon@gmail.com>
2024-11-26 03:34:30 +0000

    gcpreserve_stack is no longer conditionally compiled. Fix typo.

--- src/julia.h
+++ src/julia.h
@@ -2367,11 +2367,9 @@ typedef struct _jl_task_t {
     // uint48_t padding2_64;
     // saved gc stack top for context switches
     jl_gcframe_t *gcstack;
-#ifdef MMTK_GC
     // GC stack of objects from gc preserve regions
     // These must always be transitively pinned. Only used by MMTK.
     jl_gcframe_t *gcpreserve_stack;
-#endif
     size_t world_age;
     // quick lookup for current ptls
     jl_ptls_t ptls; // == jl_all_tls_states[tid]
